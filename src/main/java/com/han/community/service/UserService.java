2c8fc3ab2fb9f18773a8e2148913e6a024e49db1
= <hsy121345843@gmail.com>
2021-01-05 19:42:50 +0800

    sada''

--- src/main/java/com/han/community/service/UserService.java
+++ src/main/java/com/han/community/service/UserService.java
@@ -2,8 +2,14 @@ package com.han.community.service;
 
 import com.han.community.dao.UserMapper;
 import com.han.community.model.User;
+import com.han.community.util.MailClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class UserService {
@@ -11,8 +17,31 @@ public class UserService {
     @Autowired
     private UserMapper userMapper;
 
+    @Autowired   //注册邮件客户端
+    private MailClient mailClient;
+
+    @Autowired   //注册模板引擎
+    private TemplateEngine templateEngine;
+
+    //域名
+    @Value("${community.path.domain}")
+    private String domain;
+
+    //项目名
+    @Value("${server.servlet.context-path}")
+    private String contextPath;
+
+
     public User findUserById(int id){
         return userMapper.selectById(id);
+    }
+
+    public Map<String ,Object> register(User user){
+        Map<String ,Object> map = new HashMap<>();
+
+        //判断空值
+
+        return map;
     }
 
 }
---
+++ src/test/java/com/han/community/MailTests.java
@@ -0,0 +1,37 @@
+package com.han.community;
+
+import com.han.community.util.MailClient;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class MailTests {
+
+    @Autowired
+    private MailClient mailClient;
+
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    @Test
+    public void testTextMail() {
+        mailClient.sendMail("h121345843@163.com", "TEST", ":hhhhhhh");
+    }
+
+    @Test
+    public void testHtmlMail() {
+        Context context = new Context();
+        context.setVariable("username", "aaaaa");
+        String content = templateEngine.process("/mail/demo", context);
+        System.out.println(content);
+        mailClient.sendMail("h121345843@163.com","HTML",content);
+    }
+}
